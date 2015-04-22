35e7bfa5f46abcb05beb7889046ebec60064a75c
johndewolf <jdewolf06@gmail.com>
2015-04-21 21:04:34 -0400

    add ng-submit for button

--- controllers.js
+++ controllers.js
@@ -18,11 +18,15 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 
 }]);
 
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location' , 'cityService', function($scope, $location, cityService) {
   $scope.city = cityService.city;
 
   $scope.$watch('city', function() {
     cityService.city = $scope.city;
   });
+
+  $scope.submit = function() {
+    $location.path("/forecast");
+  }
 
 }]);
